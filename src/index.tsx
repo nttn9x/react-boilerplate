f16830f71eb6a0d57869af041e3053552c8b03f4
Nguyen <nguyen.nguyen@dfo.global>
2019-09-07 19:13:43 +0700

    First commit

--- src/index.tsx
+++ src/index.tsx
@@ -43,7 +43,7 @@ function createMagic() {
   // If you want your app to work offline and load faster, you can change
   // unregister() to register() below. Note this comes with some pitfalls.
   // Learn more about service workers: https://bit.ly/CRA-PWA
-  serviceWorker.unregister();
+  serviceWorker.register();
 }
 
 // this is a magic
